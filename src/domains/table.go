a7b2dfd3a67b45002564fddcd37c73c8d804a24e
smorodin-it <smorodin.it@gmail.com>
2024-06-12 22:16:17 +0300

    table domains add created at and updated at fields

--- src/domains/table.go
+++ src/domains/table.go
@@ -12,25 +12,31 @@ type Table struct {
 }
 
 type TableHeader struct {
-	ID        string `db:"table_header_id"`
-	Label     string `db:"label"`
-	IsDeleted bool   `db:"is_deleted"`
-	TableID   string `db:"table_id"`
-	ParentID  string `db:"parent_id"`
+	ID        string    `db:"table_header_id"`
+	Label     string    `db:"label"`
+	IsDeleted bool      `db:"is_deleted"`
+	TableID   string    `db:"table_id"`
+	ParentID  string    `db:"parent_id"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
 
 type TableRow struct {
-	ID        string `db:"table_row_id"`
-	Label     string `db:"label"`
-	IsDeleted bool   `db:"is_deleted"`
-	TableID   string `db:"table_id"`
+	ID        string    `db:"table_row_id"`
+	Label     string    `db:"label"`
+	IsDeleted bool      `db:"is_deleted"`
+	TableID   string    `db:"table_id"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
 
 type TableCell struct {
-	ID            string `db:"table_cell_id"`
-	TableHeaderID string `db:"table_header_id"`
-	TableRowID    string `db:"table_row_id"`
-	TableID       string `db:"table_id"`
-	Value         string `db:"value"`
-	Type          int    `db:"type"`
+	ID            string    `db:"table_cell_id"`
+	TableHeaderID string    `db:"table_header_id"`
+	TableRowID    string    `db:"table_row_id"`
+	TableID       string    `db:"table_id"`
+	Value         string    `db:"value"`
+	Type          int       `db:"type"`
+	CreatedAt     time.Time `db:"created_at"`
+	UpdatedAt     time.Time `db:"updated_at"`
 }
